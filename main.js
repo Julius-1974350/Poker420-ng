1e79f51ef26c5dc425985b8a150bb2c654beb1e0
Julius Leblanc <1974350@cstj.qc.ca>
2023-08-23 09:47:26 -0400

    Small fix

--- main.js
+++ main.js
@@ -3,11 +3,11 @@ const url = require('url');
 const path = require('path');
 
 function onReady () {
- win = new BrowserWindow({width: 600, height: 670, frame: false })
+ win = new BrowserWindow({width: 600, height: 670, frame: true })
  win.loadURL(url.format({
  pathname: path.join(
  __dirname,
- 'dist/Poker420/index.html'),
+ 'dist/poker420/index.html'),
  protocol: 'file:',
  slashes: true
  }))
